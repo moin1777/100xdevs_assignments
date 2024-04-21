2136e0f434b4904e305fcf6a4c2fd481e23ec3ec
Moin Malek <malekmoin870@gmail.com>
2024-04-21 12:42:49 +0530

    solved week3 01-middlewares assginments and updating .gitignore

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,19 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  let userId = req.headers["user-id"];
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 1;
+  }
+  if (numberOfRequestsForUser[userId] > 5) {
+    res.status(404).json({
+      msg: "more than five request was sent."
+    })
+  }
+  numberOfRequestsForUser[userId]++;
+  next();
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,8 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use((err, req, res, next) => {
+  res.status(404).json({msg: "some error occurs"});
+  errorCount++;
+});
 module.exports = app;
